ef2a5f2e058c49cf745de0b4583ee339aa4824f1
richardbonneau <richardbonneau@hotmail.com>
2019-08-09 16:22:27 -0400

    Final color palette chosen

--- lib/utils/AppColors.dart
+++ lib/utils/AppColors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color mainBackgroundColor = Color(0xFF1E4F60);
-Color statusBarColor = Color(0xfd1d4dd);
+Color mainBackgroundColor = Color(0xFF2C2E30);
+Color statusBarColor = Color(0xff202223);
 Color circleProgressBackgroundColor = Colors.white;
-Color circleProgressBarColor = Color(0xFFC83C32);
+Color circleProgressBarColor = Color(0xFF90CAF9);
 Color textColor = Colors.white;
